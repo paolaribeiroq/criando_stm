ec52ff256e02786ba54d3bb979cee5de26149804
Paola Ribeiro <paolacrsilva@gmail.com>
2024-06-07 21:25:15 -0300

    Update programaparte1.py

--- programaparte1.py
+++ programaparte1.py
@@ -10,6 +10,8 @@ st.set_page_config(
 st.header("Aula 07/06") 
 st.subheader("Adaptação do Código")
 
+"---"
+
 st.write('''Exercício 1 - Leia o arquivo empresa1.csv e apresente os resultados 3 primeiros resultados da tabela usando o método head()
 
 Exigência: deve utilizar o pacote panda''')
@@ -24,6 +26,8 @@ st.code(code, language='python')
 arquivo = "https://raw.githubusercontent.com/paolaribeiroq/criando_stm/main/empresa1.csv" 
 dfe = pd.read_csv(arquivo, sep=';') 
 st.dataframe(dfe.head(3))
+
+"---"
 
 st.write("Exercício 2 - Apresente os dados do dataframe utilizando gráficos. Crie um gráfico de linha, simples, que apresente todos os indicadores ao longo do tempo. Crie um gráfico de dispersão para ajudar na relação entre EBITDA e Lucro operacional. Crie um histograma do Lucro do período.")
 
@@ -54,6 +58,8 @@ fig, ax = plt.subplots()
 dfe["Lucro do período"].plot(kind = 'hist')
 st.pyplot(fig)
 
+"---"
+
 st.write("Exercício 3 - Agrupe a base acima por ano, em um novo dataframe, e apresente a soma das demais coluna")
 
 code = '''
@@ -62,3 +68,5 @@ st.write(dfe.groupby('Ano').mean())
 st.code(code, language='python')
 
 st.write(dfe.groupby('Ano').mean())
+
+"---"
